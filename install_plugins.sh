42a4f2337aaa3a3e6de6a3d1383b6031b8a1d3d8
Ofir Gal <ofirgal753@gmail.com>
2026-01-22 15:37:33 +0200

    virtual-desktops waybar plugin

--- install_plugins.sh
+++ install_plugins.sh
@@ -5,3 +5,6 @@ hyprpm -v add https://github.com/levnikmyskin/hyprland-virtual-desktops?tab=read
 hyprpm enable virtual-desktops
 
 hyprpm reload -n # Reload the plugins
+
+mkdir ~/.config/waybar/modules
+wget -O ~/.config/waybar/modules/libwaybar_vd.so https://github.com/givani30/waybar-vd/releases/latest/download/libwaybar_vd.so
