1ea13031db744574c692dc1c4556904bc9913441
Ofir Gal <ofirgal753@gmail.com>
2026-01-27 18:17:50 +0200

    default to 5 workspaces on launch

--- hypr/reset_vdesk_names.sh
+++ hypr/reset_vdesk_names.sh
@@ -5,7 +5,7 @@ CONFIG_LOC="$HOME/.config/hypr/UserConfigs/VirtualDesktopsNames.conf"
 cat <<EOF > $CONFIG_LOC
 plugin {
     virtual-desktops {
-        names = 1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:10
+        names = 1:1, 2:2, 3:3, 4:4, 5:5
     }
 }
 EOF
