786b7355c2f0746a855f225dc7da76603b8cb416
Ofir Gal <ofirgal753@gmail.com>
2026-01-28 11:57:56 +0200

    vdesk name for music

--- hypr/reset_vdesk_names.sh
+++ hypr/reset_vdesk_names.sh
@@ -5,7 +5,7 @@ CONFIG_LOC="$HOME/.config/hypr/UserConfigs/VirtualDesktopsNames.conf"
 cat <<EOF > $CONFIG_LOC
 plugin {
     virtual-desktops {
-        names = 1:1, 2:2, 3:3, 4:4, 5:5
+        names = 1:1, 2:2, 3:3, 4:4, 5:5, 9:9 MUSIC
     }
 }
 EOF
