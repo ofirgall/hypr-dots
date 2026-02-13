60db9b5e21815c910613f18d0743fbad42395001
Ofir Gal <ofirgal753@gmail.com>
2026-02-13 17:15:25 +0200

    moved emojis to separate file

--- hypr/UserScripts/RenameWorkspaces.py
+++ hypr/UserScripts/RenameWorkspaces.py
@@ -12,6 +12,8 @@ import re
 import subprocess
 import sys
 
+from emojis import EMOJI_RE
+
 CONFIG_LOC = os.path.expanduser("~/.config/hypr/UserConfigs/VirtualDesktopsNames.conf")
 ICON = ''
 BROWSER_ICON = ''
@@ -36,37 +38,6 @@ def get_tmux_session_status(session_name: str) -> str:
         return STATUS_ICONS.get(status, "")
     except (subprocess.TimeoutExpired, Exception):
         return ""
-
-
-EMOJI_RE = re.compile(
-    "["
-    "\U0001F600-\U0001F64F"  # emoticons
-    "\U0001F300-\U0001F5FF"  # symbols & pictographs
-    "\U0001F680-\U0001F6FF"  # transport & map
-    "\U0001F1E0-\U0001F1FF"  # flags
-    "\U0001F900-\U0001F9FF"  # supplemental symbols
-    "\U0001FA00-\U0001FA6F"  # chess symbols
-    "\U0001FA70-\U0001FAFF"  # symbols extended-A
-    "\U00002702-\U000027B0"  # dingbats
-    "\U0000FE00-\U0000FE0F"  # variation selectors
-    "\U0000200D"             # zero width joiner
-    "\U000020E3"             # combining enclosing keycap
-    "\U00002600-\U000026FF"  # misc symbols
-    "\U0000231A-\U0000231B"  # watch/hourglass
-    "\U00002934-\U00002935"  # arrows
-    "\U000025AA-\U000025AB"  # squares
-    "\U000025FB-\U000025FE"  # squares
-    "\U00002B05-\U00002B07"  # arrows
-    "\U00002B1B-\U00002B1C"  # squares
-    "\U00002B50"             # star
-    "\U00002B55"             # circle
-    "\U00003030"             # wavy dash
-    "\U0000303D"             # part alternation mark
-    "\U00003297"             # circled ideograph congratulation
-    "\U00003299"             # circled ideograph secret
-    "]+",
-    flags=re.UNICODE,
-)
 
 
 def clean_title(title: str) -> str:
